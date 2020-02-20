e48bbc787e768b5db16186e69e9c35b22bf44ca9
Aadhinana <ajibaa@gmail.com>
2020-02-20 09:53:38 +0530

    Updates

---
+++ precache-manifest.cd5394bdf1f69568d82110a72451ba9a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bcbcdd0f9beaacde053cfb87afe73ca",
+    "url": "/Aadhinana/react-burger-builder/index.html"
+  },
+  {
+    "revision": "c78914c42c0298e739af",
+    "url": "/Aadhinana/react-burger-builder/static/css/3.a601a984.chunk.css"
+  },
+  {
+    "revision": "f1f7bb0eb0ee579cb531",
+    "url": "/Aadhinana/react-burger-builder/static/css/4.12f9873e.chunk.css"
+  },
+  {
+    "revision": "b91ea1ba9c9a56412f42",
+    "url": "/Aadhinana/react-burger-builder/static/css/5.d7c05088.chunk.css"
+  },
+  {
+    "revision": "77c4456a5db766218718",
+    "url": "/Aadhinana/react-burger-builder/static/css/main.ad413bd8.chunk.css"
+  },
+  {
+    "revision": "b9570adff0f318c6b1a6",
+    "url": "/Aadhinana/react-burger-builder/static/js/2.05aabc2e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Aadhinana/react-burger-builder/static/js/2.05aabc2e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c78914c42c0298e739af",
+    "url": "/Aadhinana/react-burger-builder/static/js/3.0c0ba091.chunk.js"
+  },
+  {
+    "revision": "f1f7bb0eb0ee579cb531",
+    "url": "/Aadhinana/react-burger-builder/static/js/4.aa0b59c6.chunk.js"
+  },
+  {
+    "revision": "b91ea1ba9c9a56412f42",
+    "url": "/Aadhinana/react-burger-builder/static/js/5.40474931.chunk.js"
+  },
+  {
+    "revision": "77c4456a5db766218718",
+    "url": "/Aadhinana/react-burger-builder/static/js/main.58108a0d.chunk.js"
+  },
+  {
+    "revision": "07ab3914b5eeb80410ba",
+    "url": "/Aadhinana/react-burger-builder/static/js/runtime-main.42c7cb2c.js"
+  }
+]);
